55c66c62d510d178a39239a357d04fbbe0c6d56e
Samantha Cripps <cripps1920@gmail.com>
2026-09-15 02:24:18 +0000

    fixed alot of problems

---
+++ js/task-manager.js
@@ -0,0 +1,20 @@
+function weeklyGoal(userName, dailyGoal, bonusTasks){
+    let weeklyGoal = dailyGoal * 5;
+    let totalGoal = weeklyGoal + bonusTasks;
+    let output = `${userName}, your weekly goal is ${weeklyGoal} tasks!`;
+    document.getElementById('goal-message').innerHTML = output;
+}
+
+const goalbutton = document.getElementById('goal-btn');
+
+goalbutton.addEventListener("click", function(event) {
+    event.preventDefault();
+    let userName = document.getElementById("name").value;
+    let dailyGoal = parseFloat(document.getElementById("daily-goal").value);
+    let bonusTasks = parseFloat(document.getElementById("weekly-bonus").value);
+    weeklyGoal(userName, dailyGoal, bonusTasks);
+}); 
+
+
+
+
